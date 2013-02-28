49ef38771f5876f3f41556f927cbe96f01e4f938
Pavankumar Kondeti <pkondeti@codeaurora.org>
2013-02-28 10:19:40 +0530

    USB: android: Rate limit android enable warning
    An error message is printed when user space tries to enable/disable
    android configuration when it is already enabled/disabled.  This
    message is helpful in debugging USB composition issues.  However
    this message is getting flooded in an error condition.  If serial
    console is enabled, it is leading to watchdog bark.
    
    CRs-Fixed: 457725
    Change-Id: I2d61ac46ba3c8464bada23a64ad0d8027a4adfe9
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -1526,6 +1526,7 @@ static ssize_t enable_store(struct device *pdev, struct device_attribute *attr,
 	struct usb_composite_dev *cdev = dev->cdev;
 	struct android_usb_function *f;
 	int enabled = 0;
+	static DEFINE_RATELIMIT_STATE(rl, 10*HZ, 1);
 
 	if (!cdev)
 		return -ENODEV;
@@ -1557,7 +1558,7 @@ static ssize_t enable_store(struct device *pdev, struct device_attribute *attr,
 				f->disable(f);
 		}
 		dev->enabled = false;
-	} else {
+	} else if (__ratelimit(&rl)) {
 		pr_err("android_usb: already %s\n",
 				dev->enabled ? "enabled" : "disabled");
 	}
